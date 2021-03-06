8f9500ae60137569ca96f8249b4018b05ff150e0
Bryce Lund <bryce.lund@gmail.com>
2021-03-06 09:42:11 -0700

    add Fetch polyfill

--- src/dom/fetchOrThrow.ts
+++ src/dom/fetchOrThrow.ts
@@ -1,11 +1,10 @@
-import { env } from '../env/index';
+import fetch from 'cross-fetch';
 
 export async function fetchOrThrow(
   url: string,
   // eslint-disable-next-line no-undef
   init?: RequestInit,
 ): Promise<Response> {
-  const { fetch } = env.getEnv();
   const res = await fetch(url, init);
   if (!(res.status < 400)) {
     throw new Error(`failed to fetch: (${res.status}) ${res.statusText}, from url: ${res.url}`);
