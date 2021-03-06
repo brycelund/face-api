eeb550f46b97a650ba0c100ec80755e4d2a6a96d
Bryce Lund <bryce.lund@gmail.com>
2021-03-06 10:05:15 -0700

    rebuild

--- src/dom/fetchOrThrow.ts
+++ src/dom/fetchOrThrow.ts
@@ -7,6 +7,7 @@ export async function fetchOrThrow(
 ): Promise<Response> {
   const res = await fetch(url, init);
   if (!(res.status < 400)) {
+    console.log(`failed to fetch: (${res.status}) ${res.statusText}, from url: ${res.url}`);
     throw new Error(`failed to fetch: (${res.status}) ${res.statusText}, from url: ${res.url}`);
   }
   return res;
